d4995201bffa00303699703134f9409d531e3698
liampong <liam.pongracz@gmail.com>
2022-04-29 18:28:50 -0400

    flipcoin

--- public/main.js
+++ public/main.js
@@ -8,7 +8,19 @@ function reveal(pick) {
 
 }
 // Flip one coin and show coin image to match result when button clicked
-
+function flipCoin(event) {
+    document.getElementById("side").innerHTML = "Flipped"
+    document.getElementById("singleflip").src = "./assets/img/coin.png";
+    fetch("http://localhost:5000/app/flip/")
+        .then(res => res.json())
+        .then(data => {
+            const result = data.flip.toString();
+            setTimeout(function() {
+                document.getElementById("singleflip").src = "./assets/img/"+result+".png";
+                document.getElementById("side").innerHTML = "Result: " + result + "!";
+            }, 100);
+        })
+}
 
 // Flip multiple coins and show coin images in table as well as summary results
 // Enter number and press button to activate coin flip series
