c4c8167cbadac88c2afe0ae73c3efdb47ff5203c
liampong <liam.pongracz@gmail.com>
2022-04-30 14:21:16 -0400

    added log

--- index.js
+++ index.js
@@ -1,44 +1,127 @@
 const express = require('express')
-const app = express()
-
-const db = require("./database.js")
-
-const args = require('minimist')(process.argv.slice(2))
-
-const port = args['port'] || 5000
-
-const debug = args['debug'] || false
-
-const log = args['log'] || true
-
-const fs = require('fs')
 const morgan = require('morgan')
+const minimist = require('minimist')
+const app = express()
+const fs = require('fs')
+const db = require("./src/services/database.js")
+const res = require('express/lib/response')
+
+const args = minimist(process.argv.slice(2))
+args["help", "port", "debug", "log"]
+
+const help = (`
+server.js [options]
+--port	Set the port number for the server to listen on. Must be an integer
+            between 1 and 65535.
+--debug	If set to true, creates endlpoints /app/log/access/ which returns
+            a JSON access log from the database and /app/error which throws 
+            an error with the message "Error test successful." Defaults to 
+            false.
+--log		If set to false, no log files are written. Defaults to true.
+            Logs are always written to database.
+--help	Return this message and exit.
+`)
+
+if (args.help || args.h) {
+    console.log(help)
+    process.exit(0)
+}
+
+// port default to 5555
+const port = args.port || process.env.PORT || 5000;
+const debug = (args.debug == "true");
+const log = (args.log != "false");
 
 app.use(express.json())
-app.use(express.static('./public'))
-app.use(express.urlencoded({extended:true}))
+app.use(express.static('./public'));
+app.use(express.urlencoded({ extended: true }))
 
 const server = app.listen(port, () => {
-    console.log('App listening on port %PORT%'.replace('%PORT%',port))
-});
+    console.log('App is running on port %PORT%'.replace('%PORT%', port))
+})
 
-if (args['help']) {
-    console.log(`server.js [options]
+//Log user interaction to database
+if (log == true) {
+    const WRITESTREAM = fs.createWriteStream('access.log', { flags: 'a' });
+    app.use(morgan('combined', { stream: WRITESTREAM }));
+}
 
-    --port	Set the port number for the server to listen on. Must be an integer
-                between 1 and 65535.
+app.use((req, res, next) => {
+    let logData = {
+        remoteaddr: req.ip,
+        remoteuser: req.user,
+        time: Date.now(),
+        method: req.method,
+        url: req.url,
+        protocol: req.protocol,
+        httpversion: req.httpVersion,
+        status: res.statusCode,
+        referer: req.headers['referer'],
+        useragent: req.headers['user-agent']
+    }
+    const stmt = db.prepare('INSERT INTO accesslog (remoteaddr, remoteuser, time, method, url, protocol, httpversion, status, referer, useragent) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)');
+    const info = stmt.run(logData.remoteaddr, logData.remoteuser, logData.time, logData.method, logData.url, logData.protocol, logData.httpversion, logData.status, logData.referer, logData.useragent);
+    next();
+})
+
+// const express = require('express')
+// const app = express()
+
+// const db = require("./database.js")
+// const args = require('minimist')(process.argv.slice(2))
+
+// const port = args['port'] || 5000
+// const debug = args['debug'] || false
+// const log = args['log'] || true
+
+// const fs = require('fs')
+// const morgan = require('morgan')
+
+// app.use(express.json())
+// app.use(express.static('./public'))
+// app.use(express.urlencoded({ extended:true }))
+
+// const server = app.listen(port, () => {
+//     console.log('App listening on port %PORT%'.replace('%PORT%',port))
+// });
+
+// if (args['help']) {
+//     console.log(`server.js [options]
+
+//     --port	Set the port number for the server to listen on. Must be an integer
+//                 between 1 and 65535.
   
-    --debug	If set to 'true', creates endlpoints /app/log/access/ which returns
-                a JSON access log from the database and /app/error which throws 
-                an error with the message "Error test successful." Defaults to 
-                'false'.
+//     --debug	If set to 'true', creates endlpoints /app/log/access/ which returns
+//                 a JSON access log from the database and /app/error which throws 
+//                 an error with the message "Error test successful." Defaults to 
+//                 'false'.
   
-    --log		If set to false, no log files are written. Defaults to true.
-                Logs are always written to database.
+//     --log		If set to false, no log files are written. Defaults to true.
+//                 Logs are always written to database.
   
-    --help	Return this message and exit.
-    `);
-    process.exit(0);
+//     --help	Return this message and exit.
+//     `);
+//     process.exit(0);
+// }
+
+// if (log) {
+//     const WRITESTREAM = fs.createWriteStream('access.log', {flags: 'a'})
+//     app.use(morgan('combined',{ stream: WRITESTREAM }))
+// }
+
+
+if (debug) {
+    app.get('/app/log/access', (req, res) => {
+        try {
+            const stmt = db.prepare(`SELECT * FROM accesslog`).all();
+            res.status(200).json(stmt);
+        } catch {
+            console.error(e);
+        }
+    });
+    app.get('/app/error', (req, res) => {
+        throw new Error('Error test successful.');
+    });
 }
 
 // app.use( (req, res, next) => {
@@ -68,30 +151,19 @@ if (args['help']) {
 //         referer,
 //         useragent) values (?,?,?,?,?,?,?,?,?,?,?);
 //     `);
-//     const info = stmt.run(logdata.remoteaddr, logdata.remoteuser, logdata.time,logdata.method,
-//         logdata.url,logdata.protocol,logdata.httpversion,logdata.secure,logdata.status,logdata.referer,logdata.useragent);
-//     res.status(200).json(info);
+//     const info = stmt.run(logdata.remoteaddr,
+//         logdata.remoteuser,
+//         logdata.time,
+//         logdata.method,
+//         logdata.url,
+//         logdata.protocol,
+//         logdata.httpversion,
+//         logdata.secure,
+//         logdata.status,
+//         logdata.referer,
+//         logdata.useragent);
 //     next();
 // })
-
-if (debug) {
-    app.get('/app/log/access', (req, res) => {
-        try {
-            const stmt = db.prepare(`SELECT * FROM accesslog`).all();
-            res.status(200).json(stmt);
-        } catch {
-            console.error(e);
-        }
-    });
-    app.get('/app/error', (req, res) => {
-        throw new Error('Error test successful.');
-    });
-}
-
-if (log) {
-    const writestream = fs.createWriteStream('./access.log', {flags: 'a'})
-    app.use(morgan('combined',{stream:writestream}))
-}
 
 app.get('/app/', (req, res) => {
     res.statusCode = 200;
