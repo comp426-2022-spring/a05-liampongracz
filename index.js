27df234a8f12b3ae25548b677d9bd4be8d61cc69
liampong <liam.pongracz@gmail.com>
2022-04-29 21:16:21 -0400

    finished flipone

--- index.js
+++ index.js
@@ -41,38 +41,38 @@ if (args['help']) {
     process.exit(0);
 }
 
-app.use( (req, res, next) => {
-    let logdata = {
-        remoteaddr: req.ip,
-        remoteuser: req.user,
-        time: Date.now(),
-        method: req.method,
-        url: req.url,
-        protocol: req.protocol,
-        httpversion: req.httpVersion,
-        secure: req.secure,
-        status: res.statusCode,
-        referer: req.headers['referer'],
-        useragent: req.headers['user-agent']
-    };
-    const stmt = db.prepare(`
-        INSERT INTO accesslog (remoteaddr,
-        remoteuser,
-        time,
-        method,
-        url,
-        protocol,
-        httpversion,
-        secure,
-        status,
-        referer,
-        useragent) values (?,?,?,?,?,?,?,?,?,?,?);
-    `);
-    const info = stmt.run(logdata.remoteaddr, logdata.remoteuser, logdata.time,logdata.method,
-        logdata.url,logdata.protocol,logdata.httpversion,logdata.secure,logdata.status,logdata.referer,logdata.useragent);
-    res.status(200).json(info);
-    next();
-})
+// app.use( (req, res, next) => {
+//     let logdata = {
+//         remoteaddr: req.ip,
+//         remoteuser: req.user,
+//         time: Date.now(),
+//         method: req.method,
+//         url: req.url,
+//         protocol: req.protocol,
+//         httpversion: req.httpVersion,
+//         secure: req.secure,
+//         status: res.statusCode,
+//         referer: req.headers['referer'],
+//         useragent: req.headers['user-agent']
+//     };
+//     const stmt = db.prepare(`
+//         INSERT INTO accesslog (remoteaddr,
+//         remoteuser,
+//         time,
+//         method,
+//         url,
+//         protocol,
+//         httpversion,
+//         secure,
+//         status,
+//         referer,
+//         useragent) values (?,?,?,?,?,?,?,?,?,?,?);
+//     `);
+//     const info = stmt.run(logdata.remoteaddr, logdata.remoteuser, logdata.time,logdata.method,
+//         logdata.url,logdata.protocol,logdata.httpversion,logdata.secure,logdata.status,logdata.referer,logdata.useragent);
+//     res.status(200).json(info);
+//     next();
+// })
 
 if (debug) {
     app.get('/app/log/access', (req, res) => {
--- public/main.js
+++ public/main.js
@@ -1,3 +1,5 @@
+const { response } = require("express")
+
 // Focus div based on nav button click
 function reveal(pick) {
     document.getElementById('home').setAttribute("class", "hidden")
@@ -8,21 +10,21 @@ function reveal(pick) {
 
 }
 // Flip one coin and show coin image to match result when button clicked
-function flipCoin(event) {
-    document.getElementById("side").innerHTML = "Flipped"
+function flipCoin() {
+    document.getElementById("side").innerHTML = "Flipped";
     document.getElementById("singleflip").src = "./assets/img/coin.png";
     fetch("http://localhost:5000/app/flip/")
-        .then(res => res.json())
-        .then(data => {
-            const result = data.flip.toString();
-            setTimeout(function() {
-                document.getElementById("singleflip").src = "./assets/img/"+result+".png";
-                document.getElementById("side").innerHTML = "Result: " + result + "!";
-            }, 100);
+        .then((response) => response.json())
+        .then(function(result) {
+            document.getElementById("singleflip").src = "./assets/img/"+result.flip+".png";
+            document.getElementById("side").innerHTML = "Result: " + result.flip + "!";
         })
 }
 
 // Flip multiple coins and show coin images in table as well as summary results
+function flipCoins() {
+    
+}
 // Enter number and press button to activate coin flip series
 
 // Guess a flip by clicking either heads or tails button
